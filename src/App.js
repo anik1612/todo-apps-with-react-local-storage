26c2c211cc21b5b12224047a7a65cf4cf268932c
Anik <aniksarker1612@gmail.com>
2020-10-06 22:33:54 +0800

    project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import useLocalStorageState from 'use-local-storage-state/dist';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useForm } from 'react-hook-form';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
+  const [todos, setTodos] = useLocalStorageState('todos', [])
+  const [inpClr, setInpClr] = useState('')
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    setTodos([...todos, data.todo]);
+    setInpClr('');
+  };
+
+  const handleDeleteTodo = (index) => {
+    const restItem = todos.filter((todo, i) => i !== index)
+    setTodos(restItem);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo-container">
+      <div className='d-flex align-items-center justify-content-center text-white pt-5'>
+        <div className='border rounded p-5 bg-dark'>
+          {
+            todos.map((todo, i) => (
+              <div className='d-flex justify-content-between border-bottom mb-2'>
+                <div>
+                  <h4 key={i}>({i + 1}) {todo}</h4>
+                </div>
+                <div>
+                  <button onClick={() => handleDeleteTodo(i)} className='btn btn-sm text-white btn-danger'><FontAwesomeIcon icon={faTrashAlt} /></button>
+                </div>
+              </div>
+            )
+            )
+          }
+          <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <input type="text" className='form-control mb-3' placeholder="input your todo" value={inpClr} onChange={e => setInpClr(e.target.value)} name='todo' ref={register({ required: true })} />
+              <p className='text-white'>{errors.todo && '*Empty, Input Something'}</p>
+              <input type='submit' className='btn btn-block btn-outline-primary font-weight-bold text-white' value='Add new todo' />
+            </form>
+          </div>
+        </div>
+      </div>
+
+      {/* developer info */}
+      <div className='mt-5 text-center'>
+        <h5 className='text-info display-4'>Developer: Anik Sarker</h5>
+      </div>
     </div>
+
   );
 }
 
